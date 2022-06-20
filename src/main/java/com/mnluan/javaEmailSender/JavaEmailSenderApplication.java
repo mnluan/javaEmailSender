829913ba34020aea846d5bc6c6f6706107415b4a
Luan Nascimento <luann@id.uff.br>
2022-06-20 19:50:57 -0300

    Added Spring Boot Files

---
+++ src/main/java/com/mnluan/javaEmailSender/JavaEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.mnluan.javaEmailSender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEmailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEmailSenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mnluan/javaEmailSender/JavaEmailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnluan.javaEmailSender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEmailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
