59cc7a89dc0c5d798f576b0decb21bd18dc9bb60
Luan Nascimento <luann@id.uff.br>
2022-06-20 20:54:49 -0300

    Application for sending messages ready to use

--- src/main/java/com/mnluan/javaEmailSender/JavaEmailSenderApplication.java
+++ src/main/java/com/mnluan/javaEmailSender/JavaEmailSenderApplication.java
@@ -1,13 +1,25 @@
 package com.mnluan.javaEmailSender;
 
+import javax.mail.MessagingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class JavaEmailSenderApplication {
-
+	
+	@Autowired
+	private javaEmailSenderService emailSenderService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JavaEmailSenderApplication.class, args);
 	}
-
+	
+	@EventListener(ApplicationReadyEvent.class)
+	public void triggerEmail()throws MessagingException{
+		emailSenderService.sendMail("recipientEmail@example.com", "Messeger Here", "Subject Here", "location//from//archive.pdf");
+	}
 }
